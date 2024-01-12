c3635b47876f67548ab7517034b97eb7a92ab0ec
JOY254 <nyorgijoy@gmail.com>
2024-01-12 01:35:49 -0600

    to be completed

--- server/app.py
+++ server/app.py
@@ -2,43 +2,46 @@
 
 from flask import Flask, request
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/math', methods=['POST'])
-def math():
-    try:
-        num1 = float(request.form['num1'])
-        operation = request.form['operation']
-        num2 = float(request.form['num2'])
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)  # Print to console
+    return param
 
-        result = None
-        if operation == '+':
-            result = num1 + num2
-        elif operation == '-':
-            result = num1 - num2
-        elif operation == '*':
-            result = num1 * num2
-        elif operation == '/':
-            if num2 != 0:
-                result = num1 / num2
-            else:
-                return '<h2>Error: Division by zero</h2>'
-        elif operation == '%':
-            if num2 != 0:
-                result = num1 % num2
-            else:
-                return '<h2>Error: Modulo by zero</h2>'
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '\n'.join(str(i) for i in range(param + 1))  # Adjust range
+    return f'<h2>Count Numbers:</h2><pre>{numbers}</pre>'
+
+@app.route('/math/<float:num1>/<string:operation>/<float:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
         else:
-            return '<h2>Error: Invalid operation</h2>'
+            return '<h2>Error: Division by zero</h2>'
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return '<h2>Error: Modulo by zero</h2>'
+    else:
+        return '<h2>Error: Invalid operation</h2>'
 
-        return f'<h2>Math Result: {num1} {operation} {num2} = {result}</h2>'
-
-    except ValueError:
-        return '<h2>Error: Invalid input. Please provide valid numbers.</h2>'
+    return f'<h2>Math Result: {num1} {operation} {num2} = {result}</h2>'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
